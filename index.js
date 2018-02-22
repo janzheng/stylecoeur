3dcc51cb65f8e72b7263adf63dd1d94f4bfbf1a3
janzheng <janeazy@gmail.com>
2018-02-21 19:17:05 -0500

    reworked coeur; added to npm; folders renamed

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// for npm
+
+
+// module.exports = require("./dist/bcrypt.js");
+
+// available to other code; a demo
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
