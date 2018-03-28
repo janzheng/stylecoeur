ea9ea0405db52427497c9b0bc6f76d8f8756df3c
janzheng <janeazy@gmail.com>
2018-03-28 02:10:13 -0400

    Automated commit at 2018-03-28 06:10:13 UTC by middleman-deploy 2.0.0-alpha

--- assets/coeur/scriptcoeur/sizer.js
+++ assets/coeur/scriptcoeur/sizer.js
@@ -6,10 +6,24 @@
 // syntax: height is calculated as vh
 //         <div class="_align-vertically _size-vheight" data-vheight="40">
 $(window).load(function() {
+  function sizeup(obj) {
+    var height = $(window).height() * ($(obj).data('vheight') * 0.01);
+    $(obj).css({'height': height})
+    console.log('sizeup!!', height)
+  }
+
   $('._size-vheight').each(function() {
     // calculate vh manually, and set it dynamically
     // console.log($(this).data())
-    var height = $(window).height() * ($(this).data('vheight') * 0.01);
-    $(this).css({'height': height})
+    var _this = this;
+    sizeup(_this)
+
+    if($(this).hasClass('--live')) {
+      if(_ === undefined)
+        console.error('--live needs lodash to work')
+      else {
+        $(window).on('resize', _.throttle(function(){sizeup(_this)}, 50))
+      }
+    }
   });
 });
